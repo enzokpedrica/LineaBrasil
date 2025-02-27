2822740f2469cd98310086b9f19872992892e2c8
enzokpedrica <enzo.pedrica@gmail.com>
2025-02-27 20:55:07 -0300

    Adição da Funcionalidade de mapeamento

--- TODO.md
+++ TODO.md
@@ -6,3 +6,8 @@
 ## 12-02-2025
 (NOV) Adicionar um sistema de alteração da localização do Pallet
 (DEF) Notei um erro, o código do produto pode estar presente em mais de 1 localização logo se a requisição contém um produto onde o mesmo possui 2 localizações o pedido é duplicado. (ex: 160006033 - Parafuso - R3E - S3 - E0 - 100000.00 / -- / 160006033 - Parafuso - R3E - S3 - E2 - 100000.00) (CON)
+
+## 27-02-2025
+(NOV) Organizar a criação de um mapeamento dos itens que chegaram no dia
+(MLH) Deixar os arquivos mais organizador, criar módulos e deixar o main.py sendo o principal
+(DEF) Um erro que está acontecendo é no DropDuplicates, porque pode ocorrer de o mesmo código ser produtos diferentes, porque o que vai diferenciar um produto do outro é a Deriv, que seria basicamente a cor de cada item
--- mapeamento.py
+++ mapeamento.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import openpyxl
+from openpyxl import load_workbook
+from openpyxl.styles import Border, Side
 import subprocess
 
-colunas_esperadas = ['Produto_x']
+# Carregar o arquivo Excel
+file = "Planilha.xlsx"
+wb = load_workbook(file)
+sheet = wb.active
+
+# Definir borda padrão para remover qualquer borda anterior
+no_border = Border(left=Side(style=None), right=Side(style=None), top=Side(style=None), bottom=Side(style=None))
+
+# Iterar por todas as células e remover bordas
+for row in sheet.iter_rows():
+    for cell in row:
+        cell.border = no_border  # Remover bordas
+
+# Salvar a planilha com as bordas removidas
+wb.save("Planilha_para_manipulacao.xlsx")
 
 file = "Planilha.xlsx"
-df = pd.read_csv(file, delimiter=';', encoding='latin1', names=colunas_esperadas, skiprows=1)
+df = pd.read_excel(file, engine = "openpyxl")
 
-# print(df[['Produto','Descrição Produto','Qtde Mov']])
+print(df[['Produto','Descrição Produto','Qtde Mov']])
 
 file_map = "Lista_Map.CSV"
 df_map = pd.read_csv(file_map, delimiter=';', encoding='latin1')
@@ -19,15 +35,15 @@ df_merged = df_merged.sort_values(by='Secao')
 df_merged = df_merged[['Produto_x', 'Descrição Produto', 'Qtde Mov', 'Rua', 'Secao', 'Andar']]
 df_merged = df_merged.drop_duplicates(subset=['Descrição Produto'])
 
-df_merged.to_excel('tabela_imprimir.xlsx', index=False, engine='openpyxl')
+df_merged.to_excel('mapeamento_pre_arquivo.xlsx', index=False, engine='openpyxl')
 
-wb = openpyxl.load_workbook('tabela_imprimir.xlsx')
+wb = openpyxl.load_workbook('mapeamento_pre_arquivo.xlsx')
 ws = wb.active
 ws.title = "Tabela"
 
 # Salve o arquivo Excel
-wb.save('tabela_imprimir.xlsx')
+wb.save('mapeamento.xlsx')
 
 # Abra o arquivo Excel salvo
-subprocess.Popen(['start', 'excel.exe', 'tabela_imprimir.xlsx'], shell=True)
+subprocess.Popen(['start', 'excel.exe', 'mapeamento.xlsx'], shell=True)
 
