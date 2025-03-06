82444c4519e6ca704cdf78a642ced78b6b403fe8
enzo pedrica <enzo.pedrica@lineabr.local>
2025-03-06 13:50:58 -0300

    Atualização do IF

--- placa_fita.py
+++ placa_fita.py
@@ -39,11 +39,11 @@ codigo_produto_str = str(codigo_produto_int)
 produto = df.loc[df['Cod Produto'] == codigo_produto_int, 'Produto'].values[0]
 
 # Definir o produto
-if 150001004:
+if codigo_produto_int == 150001004:
     produto = "Fita Borda Papel 18MM"
-elif 150001005:
+elif codigo_produto_int == 150001005:
     produto = "Fita Borda Papel 28MM"
-elif 150001006:
+elif codigo_produto_int == 150001006:
     produto = "Fita Borda Papel 40MM"   
 else:
     produto = "Fita Borda PS"
