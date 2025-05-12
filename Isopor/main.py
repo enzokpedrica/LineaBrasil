41d933782f1581b7321eeb8d96c4932d9b3bf976
enzo pedrica <enzo.pedrica@lineabr.local>
2025-05-12 15:25:37 -0300

    Adição

--- Isopor/main.py
+++ Isopor/main.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 file = 'isopor.TXT'
 
-df = pd.read_csv(file, delimiter=';', encoding='latin1')
+df = pd.read_csv(file, delimiter=';',skiprows=1, encoding='latin1')
 df1 = pd.DataFrame(df)
 print(df1.columns)
